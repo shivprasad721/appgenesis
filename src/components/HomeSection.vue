e25d71044b3aa17e33518b1f5f9bf24ccacef80a
shivprasad721 <shiv.prasad@lyxelandflamingo.com>
2023-08-07 21:09:01 +0530

    first commit

---
+++ src/components/HomeSection.vue
@@ -0,0 +1,254 @@
+<template>
+  <section id="hero">
+    <div>
+      <div class="particalHeight">
+          <vue-particles
+
+        color="#250361"
+        :particleOpacity="0.7"
+        :particlesNumber="120"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#ffca58"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.5"
+        :linesDistance="150"
+        :moveSpeed="7"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+      >
+      </vue-particles>
+        </div>
+    </div>
+    <div class="courselCustom">
+    <v-carousel
+    cycle
+    height="750"
+    hide-delimiter-background
+    show-arrows="hover"
+  >
+    <v-carousel-item
+      v-for="(slide, i) in slides"
+      :key="i"
+    >
+      <v-sheet
+        style="background-color: transparent;"
+        height="100%"
+      >
+      <div style="" class="fill-height d-flex justify-center">
+        <v-row class="d-flex fill-height justify-space-between align-center " >
+          <v-col cols="12" sm="6" align="center" class="marginMobile">
+            <div class="text-sm-h2 text-h3 font-weight-bold" style="color:#250361" >
+              <div >{{ slide.heading }} </div>
+            </div>
+            <div class="text-xs-h6 text-sm-h5 mt-3">
+              {{ slide.desctiption }}
+            </div>
+            <div class="mt-3">
+              <v-btn size="x-large " color="#ffca58" class="pa-5">Get Started </v-btn>
+            </div>
+          </v-col>
+          <v-col cols="12" sm="6" align="center">
+            <v-img :src="slide.imgLink" max-width="60vh"  />
+          </v-col>
+        </v-row>
+        </div>
+      </v-sheet>
+    </v-carousel-item>
+  </v-carousel>
+</div>
+    <!-- <v-container fluid id="features" class="mt-2">
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="space-around">
+            <v-col
+              cols="12"
+              sm="4"
+              class="text-center"
+              v-for="(feature, i) in features"
+              :key="i"
+            >
+              <v-hover v-slot:default="{ hover }">
+                <v-card
+                  class="card"
+                  shaped
+                  :elevation="hover ? 10 : 4"
+                  :class="{ up: hover }"
+                >
+                  <v-img
+                    :src="feature.img"
+                    max-width="100px"
+                    class="d-block ml-auto mr-auto"
+                    :class="{ 'zoom-efect': hover }"
+                  ></v-img>
+                  <h1 class="font-weight-regular">{{ feature.title }}</h1>
+                  <h4 class="font-weight-regular subtitle-1">
+                    {{ feature.text }}
+                  </h4>
+                </v-card>
+              </v-hover>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+     -->
+    
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+      colors: [
+          
+        ],
+        slides: [
+          {
+            color:'250361',
+            heading:'Web App',
+            desctiption:'Development Company',
+            imgLink: require("@/assets/img/web-development.png")
+          },
+          {
+            heading:'Mobile App',
+            desctiption:'Development Company',
+            imgLink: require("@/assets/img/mobile-app-development.png")
+          },
+       
+        ],
+      videoId: "i8IvvHJssWE",
+      features: [
+        {
+          img: require("@/assets/img/icon2.png"),
+          title: "Design Limpo",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          img: require("@/assets/img/icon1.png"),
+          title: "Dados Seguros",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+        {
+          img: require("@/assets/img/icon3.png"),
+          title: "Código Aberto",
+          text: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        },
+      ],
+    };
+  },
+  watch: {
+    dialog(value) {
+      if (!value) {
+        this.pause();
+      }
+    },
+  },
+  methods: {
+    ready(event) {
+      this.player = event.target;
+    },
+    playing(event) {
+      // The player is playing a video.
+    },
+    change() {
+      // when you change the value, the player will also change.
+      // If you would like to change `playerVars`, please change it before you change `videoId`.
+      // If `playerVars.autoplay` is 1, `loadVideoById` will be called.
+      // If `playerVars.autoplay` is 0, `cueVideoById` will be called.
+      this.videoId = "another video id";
+    },
+    stop() {
+      this.player.stopVideo();
+    },
+    pause() {
+      this.player.pauseVideo();
+    },
+  },
+};
+</script>
+
+<style lang="css">
+.courselCustom{
+  position: absolute;
+  top:0 !important;
+  left:0 !important;
+  width:100% !important;
+}
+
+</style>
+  
+<style>
+.btn-play {
+  transition: 0.2s;
+}
+.particalHeight{
+  height:750px; 
+  background:white
+}
+@media only screen and (max-width: 600px) {
+  .marginMobile{
+    margin-top: 150px !important;
+  }
+  .particalHeight{
+    padding-top:150px !important;
+  height:750px; 
+  background:white
+}
+}
+.svg-border-waves .v-image {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  height: 3rem;
+  width: 100%;
+  overflow: hidden;
+}
+
+#hero {
+  z-index: 0;
+}
+.svg-border-waves img {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  margin-bottom: -2px;
+  z-index: -1;
+}
+
+.card {
+  min-height: 300px;
+  padding: 10px;
+  transition: 0.5s ease-out;
+}
+
+.card .v-image {
+  margin-bottom: 15px;
+  transition: 0.75s;
+}
+
+.card h1 {
+  margin-bottom: 10px;
+}
+
+.zoom-efect {
+  transform: scale(1.1);
+}
+
+.up {
+  transform: translateY(-20px);
+  transition: 0.5s ease-out;
+}
+</style>
+
+<style>
+section {
+  position: relative;
+}
+</style>
---
+++ src/components/Technology.vue
@@ -0,0 +1,217 @@
+<template>
+    <section id="about">
+      <v-container fluid>
+        <v-row align="center" justify="center">
+          <v-col cols="10">
+            <v-row align="center" justify="center">
+              <v-col cols="12" md="12">
+                <div class="font-weight-bold display-2 text-sm-h4 text-h6 " align="center" > Technologies We Work On</div>
+                <div class="mt-5 d-flex justify-center" style="border-bottom:1px solid rgb(198, 198, 198)">
+                    <div v-for="tab of tabsData" :key="tab.title" class="px-5" :class="activeTab==tab.title?'activeTab':'deactiveTab'" @click="switchTab(tab.title)" >
+                        <span >{{ tab.title }}</span> 
+                    </div>
+                </div>
+                <div v-for="tab of tabsData" :key="tab.title" class="mt-3">
+                    <v-expand-transition>
+                    <v-row v-if="activeTab == tab.title" >
+                        <v-col v-for="language of tab.languages" :key="language.language" cols="6" sm="2" class="d-flex justify-center">
+                            <div style="width:25%" class="my-8">
+                                <img :src="language.icon" width="100%">
+                                <div class="d-flex justify-center text-caption">{{ language.language }}</div>
+                            </div>
+                          </v-col>
+                    </v-row>
+                </v-expand-transition>
+                </div>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+        <v-row>  
+        </v-row>
+        <v-row align="center" justify="center">
+          <v-col cols="10">
+            <div class="font-weight-bold display-2 text-sm-h4 text-h6 " align="center" > Process We Follow</div>
+
+            <v-row align="center" justify="center" class="my-3">
+              <v-col cols="12" md="3" v-for="card of processData" :key="card.title">
+                <v-card 
+                        class="mx-auto pa-3"
+                    >
+                        <v-card-title class="d-flex justify-center">
+                        {{card.title}}
+                        </v-card-title>
+                        <v-card-subtitle   align="center">
+                        <div>{{card.subtitle}}</div>
+                        </v-card-subtitle>
+
+
+                </v-card>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+        <v-row>
+            
+        </v-row>
+      </v-container>
+    </section>
+  </template>
+  <script>
+  export default {
+        data: () => ({
+            tab: null,
+            tabsData:[
+            {
+                title:'Mobile',
+                languages:[
+                    {
+                        language:'Android',
+                        icon: require("@/assets/mobile/android.svg")
+                    }, {
+                        language:'iOS',
+                        icon: require("@/assets/mobile/apple-icon.svg")
+                    },
+                    {
+                        language:'Flutter',
+                        icon: require("@/assets/mobile/flutter.svg")
+                    },
+                    {
+                        language:'Kotlin',
+                        icon: require("@/assets/mobile/kotlin.svg")
+                    },
+                    {
+                        language:'React Native',
+                        icon: require("@/assets/mobile/react-native.svg")
+                    },
+                    {
+                        language:'Swift',
+                        icon: require("@/assets/mobile/swift-icon.svg")
+                    },
+                    {
+                        language:'Windows',
+                        icon: require("@/assets/mobile/windows.svg")
+                    }
+                ]
+            },{
+                title:'Frontend',
+                languages:[ 
+                    {
+                        language:'Vue JS',
+                        icon: require("@/assets/frontend/vuejs.svg")
+                    },
+                    {
+                        language:'Angular',
+                        icon: require("@/assets/frontend/angular.svg")
+                    },
+                    {
+                        language:'Golang',
+                        icon: require("@/assets/frontend/golang.svg")
+                    },
+                    {
+                        language:'Javascript',
+                        icon: require("@/assets/frontend/javascript.svg")
+                    },{
+                        language:'Ract JS',
+                        icon: require("@/assets/frontend/react-js.svg")
+                    },{
+                        language:'Senchatouch',
+                        icon: require("@/assets/frontend/senchatouch.svg")
+                    },{
+                        language:'Typescript',
+                        icon: require("@/assets/frontend/typescript.svg")
+                    },
+                    ]
+            },
+            {
+                title:'Backent',
+                languages:[{
+                    language:'Codeigniter',
+                    icon: require("@/assets/backend/codeigniter.svg")
+                },
+                {
+                    language:'Django',
+                    icon: require("@/assets/backend/django.svg")
+                },
+                {
+                    language:'Graphql API',
+                    icon: require("@/assets/backend/graphqlapi.svg")
+                },
+                {
+                    language:'Node JS',
+                    icon: require("@/assets/backend/nodejs.svg")
+                },
+                {
+                    language:'PHP',
+                    icon: require("@/assets/backend/php.svg")
+                },
+                {
+                    language:'Python',
+                    icon: require("@/assets/backend/python.svg")
+                }
+                ]
+            },
+          
+            
+            ],
+            processData:[
+            {
+                title:'1. Requirement Gathering',
+                subtitle:'We follow the first and foremost priority of gathering requirements, resources, and information to begin our project.'
+            },
+            {
+                title:'2. UI/UX Design',
+                subtitle:'We create catchy and charming designs with the latest tools of designing to make it a best user-friendly experience.'
+            },
+            {
+                title:'3. Prototype',
+                subtitle:'After designing, you will get your prototype, which will be sent ahead for the development process for the product.'
+            },
+            {
+                title:'4. Development',
+                subtitle:'Development of mobile application/web/blockchain started using latest tools and technologies with transparency.'
+            },
+            {
+                title:'5. Quality Assurance',
+                subtitle:'Hyperlink values quality and provides 100% bug free application with no compromisation in it.'
+            },
+            {
+                title:'6. Deployment',
+                subtitle:'After trial and following all processes, your app is ready to launch on the App store or Play Store.'
+            },
+            {
+                title:'7. Support & Maintenance',
+                subtitle:'Our company offers you all support and the team is always ready to answer every query after deployment.'
+            }
+            ],
+            activeTab:''
+        }),
+        mounted(){
+            this.activeTab= this.tabsData[0].title
+        },
+        methods:{
+            switchTab(activeTab){
+                this.activeTab=activeTab
+            }
+        }
+    }
+  </script>
+  <style scoped>
+  #about {
+    background-color: #fff;
+  }
+  .activeTab{
+    color: black  !important;
+    border-bottom: 2px solid #250361;
+    cursor: pointer;
+    font-size: 18px;
+  }
+  .deactiveTab{
+    color:gray !important;
+    cursor: pointer;
+    font-weight: 400 !important;
+    font-size: 18px;
+
+  }
+  </style>
+  
